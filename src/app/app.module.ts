0794bbae295b0baf655190cbbe2c6377635f010d
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-11-21 00:38:59 -0600

    comenzando pantalla registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './partials/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginScreenComponent,
-    RegistroUsuariosScreenComponent
+    RegistroUsuariosScreenComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
