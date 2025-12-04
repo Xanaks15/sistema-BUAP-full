6a78014603b7e1979689ec24af3868c5f9ec2e64
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-12-03 20:36:59 -0600

    creación de página de alumnos

--- sistema-buap-webapp/src/app/services/alumnos.service.ts
+++ sistema-buap-webapp/src/app/services/alumnos.service.ts
@@ -22,9 +22,9 @@ export class AlumnosService {
     private facadeService: FacadeService
   ) { }
 
-  public esquemaAlumno(){
+  public esquemaAlumno() {
     return {
-      'rol':'',
+      'rol': '',
       'matricula': '',
       'first_name': '',
       'last_name': '',
@@ -41,77 +41,77 @@ export class AlumnosService {
   }
 
   //Validación para el formulario
-  public validarAlumno(data: any, editar: boolean){
+  public validarAlumno(data: any, editar: boolean) {
     console.log("Validando alumno... ", data);
     let error: any = [];
 
-    if(!this.validatorService.required(data["matricula"])){
+    if (!this.validatorService.required(data["matricula"])) {
       error["matricula"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["first_name"])){
+    if (!this.validatorService.required(data["first_name"])) {
       error["first_name"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["last_name"])){
+    if (!this.validatorService.required(data["last_name"])) {
       error["last_name"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["email"])){
+    if (!this.validatorService.required(data["email"])) {
       error["email"] = this.errorService.required;
-    }else if(!this.validatorService.max(data["email"], 40)){
+    } else if (!this.validatorService.max(data["email"], 40)) {
       error["email"] = this.errorService.max(40);
-    }else if (!this.validatorService.email(data['email'])) {
+    } else if (!this.validatorService.email(data['email'])) {
       error['email'] = this.errorService.email;
     }
 
-    if(!editar){
-      if(!this.validatorService.required(data["password"])){
+    if (!editar) {
+      if (!this.validatorService.required(data["password"])) {
         error["password"] = this.errorService.required;
       }
 
-      if(!this.validatorService.required(data["confirmar_password"])){
+      if (!this.validatorService.required(data["confirmar_password"])) {
         error["confirmar_password"] = this.errorService.required;
       }
     }
 
-    if(!this.validatorService.required(data["fecha_nacimiento"])){
+    if (!this.validatorService.required(data["fecha_nacimiento"])) {
       error["fecha_nacimiento"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["curp"])){
+    if (!this.validatorService.required(data["curp"])) {
       error["curp"] = this.errorService.required;
-    }else if(!this.validatorService.min(data["curp"], 18)){
+    } else if (!this.validatorService.min(data["curp"], 18)) {
       error["curp"] = this.errorService.min(18);
       alert("La longitud de caracteres de la CURP es menor, deben ser 18");
-    }else if(!this.validatorService.max(data["curp"], 18)){
+    } else if (!this.validatorService.max(data["curp"], 18)) {
       error["curp"] = this.errorService.max(18);
       alert("La longitud de caracteres de la CURP es mayor, deben ser 18");
     }
 
-    if(!this.validatorService.required(data["rfc"])){
+    if (!this.validatorService.required(data["rfc"])) {
       error["rfc"] = this.errorService.required;
-    }else if(!this.validatorService.min(data["rfc"], 12)){
+    } else if (!this.validatorService.min(data["rfc"], 12)) {
       error["rfc"] = this.errorService.min(12);
       alert("La longitud de caracteres deL RFC es menor, deben ser 12");
-    }else if(!this.validatorService.max(data["rfc"], 13)){
+    } else if (!this.validatorService.max(data["rfc"], 13)) {
       error["rfc"] = this.errorService.max(13);
       alert("La longitud de caracteres deL RFC es mayor, deben ser 13");
     }
 
-    if(!this.validatorService.required(data["edad"])){
+    if (!this.validatorService.required(data["edad"])) {
       error["edad"] = this.errorService.required;
-    }else if(!this.validatorService.numeric(data["edad"])){
+    } else if (!this.validatorService.numeric(data["edad"])) {
       alert("El formato es solo números");
-    }else if(data["edad"]<18){
+    } else if (data["edad"] < 18) {
       error["edad"] = "La edad debe ser mayor o igual a 18";
     }
 
-    if(!this.validatorService.required(data["telefono"])){
+    if (!this.validatorService.required(data["telefono"])) {
       error["telefono"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["ocupacion"])){
+    if (!this.validatorService.required(data["ocupacion"])) {
       error["ocupacion"] = this.errorService.required;
     }
 
@@ -121,16 +121,32 @@ export class AlumnosService {
 
   //Aquí van los servicios HTTP
   //Servicio para registrar un nuevo alumno
-  public registrarAlumno (data: any): Observable <any>{
-    return this.http.post<any>(`${environment.url_api}/alumnos/`,data, httpOptions);
+  public registrarAlumno(data: any): Observable<any> {
+    return this.http.post<any>(`${environment.url_api}/alumnos/`, data, httpOptions);
   }
 
-  //TODO: Agregar servicio para editar alumno
+  public obtenerListaAlumnos(): Observable<any> {
+    var token = this.facadeService.getSessionToken();
+    var headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token });
+    return this.http.get<any>(`${environment.url_api}/lista-alumnos/`, { headers: headers });
+  }
+
+  //Obtener un solo alumno dependiendo su ID
+  public getAlumnoByID(idUser: Number) {
+    return this.http.get<any>(`${environment.url_api}/alumnos/?id=${idUser}`, httpOptions);
+  }
+
+  //Servicio para actualizar un alumno
+  public editarAlumno(data: any): Observable<any> {
+    var token = this.facadeService.getSessionToken();
+    var headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token });
+    return this.http.put<any>(`${environment.url_api}/alumnos-edit/`, data, { headers: headers });
+  }
 
   //Eliminar Alumno
-  public eliminarAlumno(idUser: number): Observable <any>{
+  public eliminarAlumno(idUser: number): Observable<any> {
     var token = this.facadeService.getSessionToken();
-    var headers = new HttpHeaders({ 'Content-Type': 'application/json' , 'Authorization': 'Bearer '+token});
-    return this.http.delete<any>(`${environment.url_api}/alumnos-edit/?id=${idUser}`,{headers:headers});
+    var headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token });
+    return this.http.delete<any>(`${environment.url_api}/alumnos-edit/?id=${idUser}`, { headers: headers });
   }
 }
